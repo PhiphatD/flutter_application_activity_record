d278e4f521653bd323b0f2b4922b4db716ef5603
Phiphat Deepee <Phiphat.deep@gmail.com>
2025-11-21 16:52:26 +0700

    Remove Employee model and unused import
    Deleted the employee_model.dart file and removed the unused SharedPreferences import from activity_feed_screen.dart. This cleans up unused code and dependencies.

--- flutter_application_activity_record/lib/screens/employee_screens/activities/activity_feed_screen.dart
+++ flutter_application_activity_record/lib/screens/employee_screens/activities/activity_feed_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import '../profile/profile_screen.dart';
 import 'activity_detail_screen.dart';
 
